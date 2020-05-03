229c8d5a8ca41e8f55d8fd7186d85b960869d517
Rustam-Kadyrov <recipientisreading@gmail.com>
2020-05-03 22:42:19 +0300

    Regroup test, use beforeEach for global cases and child cases

--- test/RepositoryTagRecords.test.ts
+++ test/RepositoryTagRecords.test.ts
@@ -16,100 +16,109 @@ describe('RepositoryTagRecords', function() {
     rep = null;
   });
 
-  it('save_ShouldSave_WhenCorrectEntity', function() {
-    rep.findAll(function(docs: TagRecord[]) {
-      expect(docs.length).equal(0);
-    });
+  describe("save", function() {
+    it('ShouldSave_WhenCorrectEntity', function() {
+      rep.findAll(function(docs: TagRecord[]) {
+        expect(docs.length).equal(0);
+      });
 
-    rep.save(new TagRecord("Tag1"), function(newDoc: TagRecord) {
-      expect(newDoc._id).is.match(/^[0-9a-zA-Z]{16}$/);
-      expect(newDoc.label).equal("Tag1");
-    });
-
-    rep.findAll(function(docs: TagRecord[]) {
-      expect(docs.length).equal(1);
-    });
-  })
-
-  it('save_ShouldSave10Times_When10TimesSaved', function() {
-    rep.findAll(function(docs: TagRecord[]) {
-      expect(docs.length).equal(0);
-    });
-    for (let i = 0; i < 10; i++) {
-      rep.save(new TagRecord("Tag" + i), function(newDoc: TagRecord) {
+      rep.save(new TagRecord("Tag1"), function(newDoc: TagRecord) {
         expect(newDoc._id).is.match(/^[0-9a-zA-Z]{16}$/);
-        expect(newDoc.label).equal("Tag" + i);
+        expect(newDoc.label).equal("Tag1");
       });
-    }
 
-    rep.findAll(function(docs: TagRecord[]) {
-      expect(docs.length).equal(10);
-    });
-  })
-
-  it('findById_ShouldFindParticularEntity_WhenOthersPresented', function() {
-    rep.findAll(function(docs: TagRecord[]) {
-      expect(docs.length).equal(0);
-    });
-
-    let storedItems: TagRecord[] = [];
-    for (let i = 0; i < 10; i++) {
-      rep.save(new TagRecord("Tag" + i), function(newDoc: TagRecord) {
-        storedItems.push(newDoc);
+      rep.findAll(function(docs: TagRecord[]) {
+        expect(docs.length).equal(1);
       });
-    }
+    })
 
-    rep.findAll(function(docs: TagRecord[]) {
-      expect(docs.length).equal(10);
-    });
+    it('ShouldSave10Times_When10TimesSaved', function() {
+      rep.findAll(function(docs: TagRecord[]) {
+        expect(docs.length).equal(0);
+      });
 
-    //This code snippet waits until array will be filled out
-    let repLocal = rep; //setInterval can't see variable that declared outside this block (neither let nor var). So need to redeclare it in nearest variable
-    let timeout = setInterval(function() {
-      if (storedItems.length == 10) {
-        clearInterval(timeout);
-        repLocal.findById(storedItems[2]._id, function(docs: TagRecord[]) {
-          expect(docs.length).equal(1);
-          expect(docs[0]._id).equal(storedItems[2]._id);
-          expect(docs[0].label).equal(storedItems[2].label);
+      for (let i = 0; i < 10; i++) {
+        rep.save(new TagRecord("Tag" + i), function(newDoc: TagRecord) {
+          expect(newDoc._id).is.match(/^[0-9a-zA-Z]{16}$/);
+          expect(newDoc.label).equal("Tag" + i);
         });
       }
-    }, 100)
+
+      rep.findAll(function(docs: TagRecord[]) {
+        expect(docs.length).equal(10);
+      });
+    })
+
   })
 
-  it('findByQuery_ShouldFindTagWithGivenTagPattern_WhenOthersPresented', function() {
-    rep.findAll(function(docs: TagRecord[]) {
-      expect(docs.length).equal(0);
-    });
+  describe("findById", function() {
+    it('findById_ShouldFindParticularEntity_WhenOthersPresented', function() {
 
-    rep.save(new TagRecord("One tag"), function(newDoc: TagRecord) { });
-    rep.save(new TagRecord(""), function(newDoc: TagRecord) { });
-    rep.save(new TagRecord("Another tag"), function(newDoc: TagRecord) { });
-    rep.save(new TagRecord("Another tag2"), function(newDoc: TagRecord) { });
+      let storedItems: TagRecord[] = [];
+      for (let i = 0; i < 10; i++) {
+        rep.save(new TagRecord("Tag" + i), function(newDoc: TagRecord) {
+          storedItems.push(newDoc);
+        });
+      }
 
-    rep.findAll(function(docs: TagRecord[]) {
-      expect(docs.length).equal(4);
-    });
+      //This code snippet waits until array will be filled out
+      let repLocal = rep; //setInterval can't see variable that declared outside this block (neither let nor var). So need to redeclare it in nearest variable
+      let timeout = setInterval(function() {
+        if (storedItems.length == 10) {
+          clearInterval(timeout);
+          repLocal.findById(storedItems[2]._id, function(docs: TagRecord[]) {
+            expect(docs.length).equal(1);
+            expect(docs[0]._id).equal(storedItems[2]._id);
+            expect(docs[0].label).equal(storedItems[2].label);
+          });
+        }
+      }, 100)
+    })
+  })
 
-    rep.findByQuery({ label: "Another tag" }, function(docs: TagRecord[]) {
-      expect(docs.length).equal(1);
-      expect(docs[0].label).is.equal("Another tag");
-    });
+  describe("findByQuery", function() {
+    beforeEach(() => {
+      rep.save(new TagRecord("One tag"), function(newDoc: TagRecord) { });
+      rep.save(new TagRecord(""), function(newDoc: TagRecord) { });
+      rep.save(new TagRecord("Another tag"), function(newDoc: TagRecord) { });
+      rep.save(new TagRecord("Another tag2"), function(newDoc: TagRecord) { });
+    })
 
-    rep.findByQuery({ label: /^Another.*/ }, function(docs: TagRecord[]) {
-      expect(docs.length).equal(2);
-      expect(docs.map(rec => { return rec.label })).to.have.members(["Another tag", "Another tag2"]);
-    });
+    it('ShouldFindTagByStrictEquality_WhenOthersPresented', function() {
 
-    rep.findByQuery({ label: /^.*tag.*$/ }, function(docs: TagRecord[]) {
-      expect(docs.length).equal(3);
-      expect(docs.map(rec => { return rec.label })).to.have.members(["One tag", "Another tag", "Another tag2"]);
-    });
+      rep.findByQuery({ label: "Another tag" }, function(docs: TagRecord[]) {
+        expect(docs.length).equal(1);
+        expect(docs[0].label).is.equal("Another tag");
+      });
 
-    rep.findByQuery({ label: /^(Another|One) tag$/ }, function(docs: TagRecord[]) {
-      expect(docs.length).equal(2);
-      expect(docs.map(rec => { return rec.label })).to.have.members(["One tag", "Another tag"]);
-    });
+    })
+
+    it('ShouldFindTagByEmptyString_WhenOthersPresented', function() {
+
+      rep.findByQuery({ label: "" }, function(docs: TagRecord[]) {
+        expect(docs.length).equal(1);
+        expect(docs[0].label).is.equal("");
+      });
+
+    })
+
+    it('ShouldFindTagByRegexp_WhenOthersPresented', function() {
+
+      rep.findByQuery({ label: /^Another.*/ }, function(docs: TagRecord[]) {
+        expect(docs.length).equal(2);
+        expect(docs.map(rec => { return rec.label })).to.have.members(["Another tag", "Another tag2"]);
+      });
+
+      rep.findByQuery({ label: /^.*tag.*$/ }, function(docs: TagRecord[]) {
+        expect(docs.length).equal(3);
+        expect(docs.map(rec => { return rec.label })).to.have.members(["One tag", "Another tag", "Another tag2"]);
+      });
+
+      rep.findByQuery({ label: /^(Another|One) tag$/ }, function(docs: TagRecord[]) {
+        expect(docs.length).equal(2);
+        expect(docs.map(rec => { return rec.label })).to.have.members(["One tag", "Another tag"]);
+      });
+    })
   })
 
 })
