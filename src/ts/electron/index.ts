3920fd8d86fdbb7ca2236e802c4d31ace819a39a
Rustam-Kadyrov <recipientisreading@gmail.com>
2020-05-13 01:21:19 +0300

    Added possibility to debug electron application

--- src/ts/electron/index.ts
+++ src/ts/electron/index.ts
@@ -1,5 +1,6 @@
 import { app, BrowserWindow } from 'electron';
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
+declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: any;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -11,6 +12,10 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     height: 600,
     width: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY
+    }
   });
 
   // and load the index.html of the app.
